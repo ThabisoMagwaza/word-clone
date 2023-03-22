ffebc680e066fac988bb0f33d8f71d6143e0a576
Thabiso Magwaza <magwazathabiso9@gmail.com>
2023-03-22 05:06:16 +0200

    exercise 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,6 +8,7 @@ import Input from '../Input';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
@@ -20,7 +21,7 @@ function Game() {
 
   return (
     <>
-      <Guesses guesses={guesses} />
+      <Guesses guesses={guesses} answer={answer} />
       <Input addGuess={addGuess} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const results = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(5).map((_, index) => (
-        <span key={index} className="cell">
+        <span
+          key={index}
+          className={`cell ${(results && results[index].status) || ''}`}
+        >
           {value ? value[index] : ''}
         </span>
       ))}
